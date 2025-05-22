c9dafd09dc95c39a21db14cfd9bf7c457bea16ad
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-05-22 17:17:45 +0300

    feat: new components created

---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,6 @@
+function FunctionalComponent() {
+    const title = 'is a FunctionalComponent';
+    return <h1 className="text-center text-xl font-bold mt-12">{title}</h1>;
+}
+
+export default FunctionalComponent;
