ac774b2554869d4eba9f02f7468eb8533fe3da5a
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-06-10 17:45:36 +0300

    feat: created a component that cheks if the browser is online

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 import Layout from "./components/Layout.tsx";
+import OnlineStatus from "./components/OnlineStatus.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
 // import CounterWithReducer from "./components/CounterWithReducer.tsx";
 // import Todo from "./components/Todo/Todo.tsx";
-import NameChanger from "./components/NameChanger.tsx";
+// import NameChanger from "./components/NameChanger.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
-// import NameChanger from "./components/NameChanger.tsx";
+import NameChanger from "./components/NameChanger.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
 
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
@@ -41,6 +42,7 @@ function App() {
             {/*<CounterAdvancedWithCustomHook></CounterAdvancedWithCustomHook>*/}
             {/*<CounterWithReducer></CounterWithReducer>*/}
             {/*<Todo></Todo>*/}
+            <OnlineStatus/>
             <NameChanger></NameChanger>
         </Layout>
     </>
